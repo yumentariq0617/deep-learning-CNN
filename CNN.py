4e27ba48382d1a9c91d63d03f0026d8ddf2825ca
yumentariq0617 <yumentariq0@gmail.com>
2024-08-31 23:18:11 +0500

    Add files via upload

---
+++ CNN.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models, datasets
+
+
+(x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
+
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+history = model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+print(f'Test accuracy: {test_acc}')
